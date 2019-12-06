1ee112de64496ce08ee7b119db325077faa91176
Danny Sievers <danjsievers@gmail.com>
2019-12-06 17:06:29 -0600

    remove incorrect vars

--- organization/variables.tf
+++ organization/variables.tf
@@ -6,14 +6,3 @@ variable "aws_service_access_principals" {
   type = list(string)
   default = []
 }
-
-variable "org_id" {}
-
-variable "region" {
-  default = "us-central1"
-}
-
-variable "gcp_services" {
-  type = list(string)
-  default = []
-}
